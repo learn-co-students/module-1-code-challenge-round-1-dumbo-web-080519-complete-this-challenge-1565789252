74c36038c078c25955d72572a58c33b7a744bba7
Jmendoza173 <Joelmendza173@gmail.com>
2019-08-14 10:35:25 -0400

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,7 +6,41 @@ class Customer
     @last_name  = last_name
   end
 
-  def full_name
+  def add_review(restaurant, content, rating)
+    if rating >= 1 && rating <= 5
+      Review.new(content, rating, self, restaurant)
+    else
+      puts "Cannot create review because rating must be between 1 - 5"
+    end
+  end
+
+  def customer_review
+      Review.all.select{|review| review.customer == self}
+  end
+
+  def num_reviews
+      self.customer_review.count
+  end
+
+  def restaurant 
+      restaurant_reviewed = self.customer_review.map {|customer| customer.restaurant}
+      restaurant_reviewed.uniq  
+  end
+
+  def full_name(first_name, last_name)
     "#{first_name} #{last_name}"
+  end
+
+  def self.find_by_name(name)
+      fullname = name.split(' ')
+      self.all.find {|name| name.first_name == fullname[0] && name.last_name == fullname[1]}
+  end
+
+  def self.find_by_first_name(name)
+      self.all.select{|name| name.first_name == name}
+  end
+
+  def self.all_name
+      self.all.map {|name| full_name(name.first_name,name.last_name)}
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,42 @@
 class Review
+    attr_reader :content, :rating, :customer, :restaurant 
+    @@all = []
+
+    def initialize(content , rating, customer, restaurant)
+        @cotnent = content
+        @rating = rating
+        @customer = customer
+        @restaurant = restaurant
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def find_review
+        self.all.find {|review| review.content == self}
+    end
+
+    def customer
+        self.find_review.customer
+    end
+
+    def Restaurant
+        self.find_review.restaurant
+    end
+
+    def find_restaurant
+        self.all.select {|content| content.restaurant == self}
+    end
+
+    def rating
+        self.find_restaurant.rating
+    end
+
+    def content
+        self.find_review.content
+    end
   
 end
 
