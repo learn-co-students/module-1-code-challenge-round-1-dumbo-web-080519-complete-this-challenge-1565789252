742f91896e2a88400090331ecb6850b67fb5ace7
Tate <tateshepard@yahoo.com>
2019-08-14 10:35:30 -0400

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
 class Review
+
+    attr_reader :restaurant, :content, :rating
+    @@all = []
+
+    def initialize(restaurant, content, rating)
+        @restaurant = restaurant
+        @content = content
+        @rating = rating
+        @@all << self
+    end
+
+    # def add_review(restaurant, content, rating)
+ 
+    # end
+
+    def self.all
+        @@all
+      end
   
 end
 
