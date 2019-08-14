0b966747763f005d59ed653aa6586bb4ac4fa794
github username <github email address>
2019-08-14 10:35:27 -0400

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,46 @@
 class Customer
   attr_reader :first_name, :last_name
-
+@@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(restaurant, content, rating, self)
+  end
+
+  def all_my_reviews
+    Review.all.select {|review| review.customer == self}
+  end
+
+  def num_reviews
+    self.all_my_reviews.count
+  end
+
+  def restaurants
+    rest_array= self.all_my_reviews.map {|my_reviews|my_reviews.restaurant}
+    rest_array.uniq
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|customer| customer.full_name == name}
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select {|customer| customer.first_name == name}
+  end
+
+  def self.all_names
+    self.all.select {|customer|customer.full_name}
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review
-  
-end
+  attr_reader :restaurant, :content, :rating, :customer
+  @@all = []
 
+  def initialize (restaurant, content, rating, customer)
+    @restaurant = restaurant
+    @content = content
+    @rating = rating
+    @customer = customer
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
