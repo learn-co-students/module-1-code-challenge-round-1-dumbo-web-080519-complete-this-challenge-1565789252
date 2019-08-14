8ba868c5f612a4b473f81f955ff07601ca327499
Brandon James <brandonj2858@gmail.com>
2019-08-14 10:35:14 -0400

    not done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,37 @@
 class Customer
   attr_reader :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def num_reviews
+    Review.all.select do |review|
+      review.customer == self
+    end.count
+  end
+
+  def restaurants
+    Restaurant.all.select do |restaurant|
+      reviews_by_customor = Review.all.map do |review|
+        review.customer
+      end
+      reviews_by_customor ==
+    end
+  end
+
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,16 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+rachel = Customer.new("Rachel", "Francis")
+brandon = Customer.new("Brandon", "James")
+
+carmines = Restaurant.new("Carmines")
+joes = Restaurant.new("Joes")
+
+r1 = Review.new(joes, rachel, "Unproffesional", 2)
+r2 = Review.new(carmines, brandon, "Family friendly and also delicious", 5)
+r3 = Review.new(carmines, rachel, "Okay", 4)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
