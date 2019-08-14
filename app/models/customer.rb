265246f1abc65ac5c302e88b614ad1693ec2b014
MichelleNguy <plasticwatering@gmail.com>
2019-08-14 10:35:19 -0400

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,48 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def reviews 
+    Review.all.select {|review| review.customer == self}
+  end
+
+  def num_reviews
+    reviews.size
+  end
+
+  def restaurants
+    (reviews.map {|review| review.restaurant}).uniq
+  end
+
+  def add_review(restaurant, rating, content)
+    Review.new(self, restaurant, rating, content)
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.find_by_name(full_name)
+    self.all.find {|customer| customer.full_name == full_name}
+  end
+
+  def self.find_all_by_first_name(first_name)
+    self.all.select {|customer| customer.first_name == first_name}
+  end
+
+  def self.all_names
+    self.all.map {|customer| customer.full_name}
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,43 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    (reviews.map {|review| review.customer}).uniq
+  end
+
+  def total_rating
+    (reviews.map {|review| review.rating}).sum
+  end
+
+  def average_star_rating
+    total_reviews = reviews.size
+    average = (total_rating.to_f / total_reviews.to_f)
+    ## for restaurants with no reviews/rating, we'll return 0
+    average.is_a?(Integer) ? average : 0
+  end
+
+  def longest_review
+    longest_review = reviews.max_by {|review| review.content.length}
+    longest_review ? longest_review.content : "No reviews yet!"
+  end
+
+  def self.find_by_name(name)
+      self.all.find {|restaurant| restaurant.name == name}
+  end
+
+  def self.all
+    @@all
   end
 
 end
