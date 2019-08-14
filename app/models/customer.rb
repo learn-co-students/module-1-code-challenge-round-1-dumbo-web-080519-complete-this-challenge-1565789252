9f175eb4ca84b5d98076448513a502c02aaeeb4e
Josh Nho <joshnho@gmail.com>
2019-08-14 10:35:26 -0400

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,54 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def add_review(restaurant, rating, content)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def num_reviews
+    Review.all.select do |review|
+      review if review.customer == self
+    end.compact.count
+  end
+
+  def restaurants
+    Review.all.map do |review|
+      review.restaurant if review.customer == self
+    end.compact.uniq
+  end
+
+  def self.find_by_name(name)
+    Customer.all.find do |customer|
+      customer if customer.full_name == name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    Customer.all.select do |customer|
+      customer if customer.first_name == name
+    end
+  end
+
+  def self.all_names
+    Customer.all.map do |customer|
+      customer.full_name
+    end
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,54 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def customers
+    Review.all.map do |review|
+      review.customer if review.restaurant == self
+    end.compact.uniq
+  end
+
+  def reviews
+    Review.all.map do |review|
+      review if review.restaurant == self
+    end.compact
+  end
+
+  def average_star_rating
+    total_rating = all_ratings.sum
+    num_of_ratings = all_ratings.count
+    total_rating / num_of_ratings
+  end
+
+  def longest_review
+    longest_review = ""
+    Review.all.each do |review|
+      if review.content.length > longest_review.length
+        longest_review = review.content
+      end
+    end
+    longest_review
+  end
+
+  def all_ratings
+    Review.all.map do |review|
+      review.rating if review.restaurant == self
+    end.compact
+  end
+
+  def self.find_by_name(name)
+    Restaurant.all.find do |restaurant|
+      restaurant if restaurant.name == name
+    end
+  end
 end
