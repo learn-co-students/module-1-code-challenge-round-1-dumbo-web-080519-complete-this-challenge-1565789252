eddf250301a32b3bae1b4158081549dda3146f1a
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-08-14 10:37:54 -0400

    Hope I pass

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,45 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name (name)
+    Restaurant.all.find do |name|
+      name == name
+    end
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant_name == self
+    end
+  end
+
+  def customers
+    self.reviews.map do |review|
+      review.customer
+    end
+  end
+
+
+
+  def restaurant_average_star
+    self.reviews.map do |review|
+      review.rating % 100.0
+    end
+  end
+
+  def longest_review
+   
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,16 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+derya = Customer.new("Derya", "Tanriverdi")
+kevin = Customer.new("Kevin", "Molduene")
+damla = Customer.new("Damla", "Gul")
+
+
+r1 = Restaurant.new("Amber Steak House")
+r2 = Restaurant.new("Jojo")
+
+review2 = Review.new(derya, r1, "Amazing!", 5)
+review2 = Review.new(kevin, r2, "So expensive!", 1)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
